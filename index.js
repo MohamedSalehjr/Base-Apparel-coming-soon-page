f25d724e77250c28f856d4707185f6cc50b92593
Mohamed  Saleh <mohamedsaleh@Mohameds-MacBook-Pro.local>
2022-02-14 22:08:01 +0000

    mobile design responsiveness

---
+++ index.js
@@ -0,0 +1,12 @@
+// $( ".hero" ).remove();
+
+// function toggleID(){
+//     var wid = $(window).width();
+//     if(wid > 768 && wid < 1200){
+//     }
+//     else if(wid >= 1200){
+//     }
+//     }
+//     $(window).resize(function(){
+//         $( ".hero" ).remove();
+//     });
